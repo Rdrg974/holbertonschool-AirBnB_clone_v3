f2a208559da4f8bc70ee2495e0a037cc6abcebfe
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-04-18 22:01:05 +0200

    Unittest file and db storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -113,3 +113,23 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_get(self):
+        """Test that get returns the correct object"""
+        storage = FileStorage()
+        new_state = State(name="California")
+        new_state.save()
+        state_id = new_state.id
+        state = storage.get(State, state_id)
+        self.assertIs(state, new_state)
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_count(self):
+        """Test that count returns the number of objects in storage"""
+        storage = FileStorage()
+        count = storage.count()
+        new_state = State(name="California")
+        new_state.save()
+        new_count = storage.count()
+        self.assertEqual(count + 1, new_count)
