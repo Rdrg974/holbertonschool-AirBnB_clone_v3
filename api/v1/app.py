d5e2d70d084d51f3e0cbdebbe4b9b9c46947028b
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-04-18 12:00:34 +0200

    Fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,22 +7,22 @@ from models import storage
 from os import getenv
 
 
-flk = Flask(__name__)
-flk.register_blueprint(app_views)
+app = Flask(__name__)
+app.register_blueprint(app_views)
 
 
-@flk.teardown_appcontext
+@app.teardown_appcontext
 def close_storage():
     """Close storage"""
     storage.close()
 
 
-@flk.errorhandler(404)
+@app.errorhandler(404)
 def page_not_found():
     """Page not found"""
     return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
-    flk.run(host=getenv('HBNB_API_HOST'),
+    app.run(host=getenv('HBNB_API_HOST'),
             port=getenv('HBNB_API_PORT'), threaded=True)
