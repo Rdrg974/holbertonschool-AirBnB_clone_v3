6eff4c5505431917bd42ec7bb2212445f85a32f4
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-04-18 09:15:28 +0200

    Improve storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,15 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """retrieve one object"""
+        key = cls.__name__ + '.' + id
+        try:
+            return self.all()[key]
+        except KeyError:
+            return None
+
+    def count(self, cls=None):
+        """count the number of objects in storage"""
+        return len(self.all(cls))
