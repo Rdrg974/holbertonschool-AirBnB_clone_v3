1ef6e27486d2181d14b9e1234835298d94299391
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-04-18 11:06:58 +0200

    Status of your API

---
+++ api/v1/app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+
+from os import getenv
+from models import storage
+from flask import Flask, jsonify
+from api.v1.views import app_views
+
+
+flk = Flask(__name__)
+flk.register_blueprint(app_views)
+
+
+@flk.teardown_appcontext
+def close_storage():
+    """Close storage"""
+    storage.close()
+
+
+@flk.errorhandler(404)
+def page_not_found():
+    """Page not found"""
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == "__main__":
+    flk.run(host=getenv('HBNB_API_HOST'),
+            port=getenv('HBNB_API_PORT'), threaded=True)
