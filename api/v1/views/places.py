a9577a5f69af942de73a0710ca4fdf0b51ce3ed7
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-04-19 14:15:09 +0200

    Forget the fct update

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Places view for API v1"""
+"""Place view for API v1"""
 
 from api.v1.views import app_views
 from flask import abort, jsonify, request
@@ -47,7 +47,7 @@ def create_place(city_id):
     """Creates a Place object"""
     city = storage.get(City, city_id)
     if city is None:
-        abort(404) 
+        abort(404)
     if not request.is_json:
         return jsonify({"error": "Not a JSON"}), 400
     new_dict = request.get_json()
@@ -62,3 +62,20 @@ def create_place(city_id):
     place = Place(**new_dict)
     place.save()
     return jsonify(place.to_dict()), 201
+
+
+@app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
+def update_place(place_id):
+    """Updates a Place object"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    if not request.is_json:
+        return jsonify({"error": "Not a JSON"}), 400
+    new_dict = request.get_json()
+    for key, value in new_dict.items():
+        if key not in ['id', 'user_id', 'city_id',
+                       'created', 'updated']:
+            setattr(place, key, value)
+    place.save()
+    return jsonify(place.to_dict())
